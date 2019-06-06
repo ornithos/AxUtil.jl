a535b6f54a818336eca4f4db80b7c46cfa7fac94
Alex Bird <abird@turing.ac.uk>
2019-06-06 14:11:03 +0000

    fixed cuda make_lt

--- src/cuda.jl
+++ src/cuda.jl
@@ -1,8 +1,8 @@
 module CUDA
 
 using ..CuArrays
-using ..ArgCheck
-import ..Math: make_lt, make_lt_strict
+import ..Math: @argcheck
+import ..Math: make_lt, make_lt_strict, make_lt!, make_lt_strict!
 
 function make_lt(x::CuArray{T,1}, d::Int)::CuArray{T,2} where T <: Real
     @argcheck (length(x) == Int(d*(d+1)/2))
